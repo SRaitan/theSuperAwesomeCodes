28d4ab4be8b0e8aeaa65aff982f6080d1165df58
SRaitan <raitan@g.jct.ac.il>
2017-04-26 09:49:55 +0300

    webchat

--- WebChat/src/com/company/Servlet.java
+++ WebChat/src/com/company/Servlet.java
@@ -1,13 +1,23 @@
 package com.company;
 
+import javax.servlet.ServletException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Servlet extends javax.servlet.http.HttpServlet {
-    private String message;
+    int counter = 0;
+    private List<String> messages;
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        messages=new ArrayList<>();
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
@@ -27,13 +37,35 @@ public class Servlet extends javax.servlet.http.HttpServlet {
             return;
         switch (action) {
             case "send":
-                message = qs.get("msg");
+                String message = qs.get("msg");
+                if(message == null) return;
+                messages.add(message);
+                response.getWriter().write("ok");
                 break;
             case "read":
-                response.getWriter().write(message);
+                int from = 0;
+                String fromString = qs.get("from");
+                if(fromString == null)
+                    return;
+                try{
+                    from = Integer.valueOf(fromString);
+                }catch (Exception ex){
+                    return;
+                }
+                if(from < 0)
+                    return;
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i=from; i < messages.size(); i++) {
+                    stringBuilder.append(messages.get(i) + "&");
+                }
+                if(messages.size() > 0){
+                    stringBuilder.delete(stringBuilder.length()-1, stringBuilder.length());
+                }
+                //if(messages.size() != 0 && from < messages.size())
+                //    stringBuilder.append(messages.get(messages.size()-1));
+                response.getWriter().write(stringBuilder.toString());
                 break;
             default:
-                return;
         }
 
     }
