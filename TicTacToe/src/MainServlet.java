9eff5a710036e9e16fa02ad6e37d6fdaa2bf00e8
SRaitan <raitan@g.jct.ac.il>
2017-04-26 15:59:47 +0300

    ticTac

---
+++ TicTacToe/src/MainServlet.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+/**
+ * Created by hackeru on 4/26/2017.
+ */
+public class MainServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
